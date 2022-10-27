5c7a49aee0c70c1492fc595dfdf252f3be60e667
saidwrick <cedric.a.fung@gmail.com>
2022-10-27 07:06:29 -0400

    loading screen only on initial load

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -6,7 +6,10 @@ import Navbar from "./components/Navbar.js";
 
 const RouteSwitch = () => {
 
-    const [loading, setLoading] = useState(true);
+    // loading screen only if server hasn't been used for 14 mins
+    const [loading, setLoading] = useState(
+        Date.now() - localStorage.lastLog >= 840000 || !localStorage.lastLog ? true : false
+    );
 
     const api = process.env.REACT_APP_API_URL
 
@@ -33,8 +36,10 @@ const RouteSwitch = () => {
     }
 
     useEffect(() => {
-        queryApi();
-
+        if (loading){
+            queryApi();
+        }
+        localStorage.lastLog = Date.now();
     },[]);
 
     if (loading){
